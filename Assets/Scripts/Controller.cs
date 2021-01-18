71b26ba07da5108c4ea84c86a8a38a35fbc7eb53
dafors <da_fors@hotmail.com>
2021-01-18 14:27:25 +0100

    Add script files that we will probably need.

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+  /*
+   Controller has at least the following methods:
+- merge(Merge runes inventory)-> rune
+- purchase runes() -> 4 uncommon runes
+- Move(from inventory, to inventory, rune)
+
+   */
+}
