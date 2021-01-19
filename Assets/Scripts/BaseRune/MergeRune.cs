79f18e30a14300935dd6410ef54f189ab92cefee
TrushBug <73204786+TrushBug@users.noreply.github.com>
2021-01-19 16:20:17 +0100

    Added prototype for inventory and also get Runes button prototype and  LT_test scene
    - Can be foun in

--- Assets/Scripts/BaseRune/MergeRune.cs
+++ Assets/Scripts/BaseRune/MergeRune.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using Random = UnityEngine.Random;
 
 namespace BaseRune {
+    //Breaks
     public class MergeRune : RuneClass{
         public static Rune Merge(List<Rune> runes) {
+            const int mergeAmount = 1;
             Rune.RarityEnum baseRarity = runes[0].Rarity;
 
             int chanceForUpgrade = runes.Count switch {2 => 20, 3 => 55, 4 => 95, _ => 0};
             if (Random.Range(0, 100) < chanceForUpgrade && Enum.GetNames(typeof(Rune.RarityEnum)).Length != (int)runes[0].Rarity)
                 baseRarity = (Rune.RarityEnum)Enum.GetValues(typeof(Rune.RarityEnum)).GetValue((int)baseRarity + 1);
             
-            return new Rune(baseRarity, runes[Random.Range(0, runes.Count)].Stat);
+            return new Rune(baseRarity, runes[Random.Range(0, runes.Count)].Stat, mergeAmount);
         }
     }
 }
