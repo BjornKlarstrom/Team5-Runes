dacfbc0df9bbb5db5c05840dd1bfa945bef90cf7
dafors <da_fors@hotmail.com>
2021-01-19 22:05:36 +0100

    Fix drag and drop. Save as new scene so I dont clash with changes @BjornKlarstrom made in secret.

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -4,35 +4,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(CanvasGroup))]
 public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     [SerializeField] private Canvas _canvas;
-    private RectTransform _rectTransform;
     public RuneSO runeBeingDragged;
+    private RectTransform _rectTransform;
+    private CanvasGroup _canvasGroup;
+    private float _speed = 1500f;
     
     private void Awake()
     {
         _rectTransform = GetComponent<RectTransform>();
+        _canvasGroup = GetComponent<CanvasGroup>();
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        _canvasGroup.blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         _rectTransform.anchoredPosition += eventData.delta / _canvas.scaleFactor;
+        
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        _canvasGroup.blocksRaycasts = true;
         
+        if (!eventData.hovered.Find(
+            gameObject => gameObject != null && gameObject.GetComponent<RuneSlot>() != null))
+        {
+            Debug.Log("Moving to previous slot");
+            StartCoroutine(MoveToPreviousSlot());            
+        }
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         
+    }
+
+    private IEnumerator MoveToPreviousSlot()
+    {
+        while ((transform.parent.position - transform.position).magnitude > 2f)
+        {
+            Vector3 dir = (transform.parent.position - transform.position).normalized;
+            transform.position += Time.deltaTime * _speed * dir;
+            yield return null;
+        }
+
+        transform.position = transform.parent.position;
+        transform.parent.GetComponent<RuneSlot>().AddRune(runeBeingDragged);
     }
 
     // Update is called once per frame
