d3ce5ea88bde50e7489253668900a76836e3a058
TrushBug <73204786+TrushBug@users.noreply.github.com>
2021-01-20 13:55:24 +0100

    LT_test2

---
+++ Assets/Scripts/BaseRune/CreateDragable.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace BaseRune {
+    public class CreateDragable : MonoBehaviour
+    {
+        public GameObject dragableGameObject;
+        public Sprite dragableSprite;
+        public RuneClass.Rune dragableRune;
+
+    }
+}
